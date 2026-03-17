68d805f837a6d746f4902a106b600002d0805f87
nenis11andres <nenis11andres@gmail.com>
2026-03-17 17:31:04 +0100

    Añadimos todo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import { useEffect, useState } from 'react';
-
+import { useEffect, useState } from "react";
 
 function App() {
-const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
 
-useEffect(() => {
-fetch('http://localhost:8000/api/hello')
-.then(res => res.json())
-.then(data => setMessage(data.message));
-}, []);
-
-
-return (
-<div style={{ padding: 20 }}>
-<h1>React + Symfony</h1>
-<p>{message}</p>
-</div>
-);
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>React + Symfony</h1>
+      <p>{message}</p>
+    </div>
+  );
 }
-
 
 export default App;
